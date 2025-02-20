6abb040e2e2149532b073d4c6bb3175ef35f6191
IkalSama <joestarjosuke28@gmail.com>
2025-02-20 16:11:24 +0800

    initial commit4

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -14,6 +14,7 @@ export const generateToken = async(user, res) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         maxAge: 3 * 24 * 60 * 60 * 1000,
+        sameSite: "None",
     })
 
     return token;
